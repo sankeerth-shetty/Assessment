6699945d53f999f594eab7b7f7aaf2b6b6d35352
sankeerth-shetty <shetty.sankeerth@gmail.com>
2022-12-25 23:42:02 +0530

    assessment test 2

--- Assessment Test 2/js/js1.js
+++ Assessment Test 2/js/js1.js
@@ -2,10 +2,10 @@ function btnclick(show){
     var menu = document.getElementById("nav-buttons");
     // console.log(menu);
 
-    if( menu.style.display == "none" ){
-        menu.style.display = "block";
+    if( menu.style.display === "block" ){
+        menu.style.display = "none";
     }
     else{
-        menu.style.display = "none";
+        menu.style.display = "block";
     }
 }
